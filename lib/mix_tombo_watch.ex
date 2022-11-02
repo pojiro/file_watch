6919dcb2cb3c71a48036a730605ced2a0b297dd8
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-11-02 15:38:24 +0900

    Initial commit

---
+++ lib/mix_tombo_watch.ex
@@ -0,0 +1,18 @@
+defmodule MixTomboWatch do
+  @moduledoc """
+  Documentation for `MixTomboWatch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MixTomboWatch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mix_tombo_watch_test.exs
@@ -0,0 +1,8 @@
+defmodule MixTomboWatchTest do
+  use ExUnit.Case
+  doctest MixTomboWatch
+
+  test "greets the world" do
+    assert MixTomboWatch.hello() == :world
+  end
+end
