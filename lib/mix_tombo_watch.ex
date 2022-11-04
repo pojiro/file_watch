856b3bb98eb84890b57a0fa4f33119474ec16ee1
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-11-04 16:45:27 +0900

    Change CLI type from `mix task` to `escript`
    WHY: `mix task` cannot include dependencies

--- lib/mix_tombo_watch.ex
+++ lib/mix_tombo_watch.ex
@@ -15,4 +15,12 @@ defmodule MixTomboWatch do
   def hello do
     :world
   end
+
+  def main(_args) do
+    {:ok, _apps} = Application.ensure_all_started(:mix_tombo_watch)
+
+    if not (Code.ensure_loaded?(IEx) && IEx.started?()) do
+      Process.sleep(:infinity)
+    end
+  end
 end
--- lib/mix_tombo_watch/fs_subscriber.ex
+++ lib/mix_tombo_watch/fs_subscriber.ex
@@ -1,8 +1,13 @@
 defmodule MixTomboWatch.FsSubscriber do
   use GenServer
 
+  require Logger
+
+  @wrap_command_sh_file_name "wrap_command.sh"
+  @wrap_command_sh File.read!(Path.join("priv", @wrap_command_sh_file_name))
+
   defmodule State do
-    defstruct config: %MixTomboWatch.Config{}, ports: []
+    defstruct config: %MixTomboWatch.Config{}, port_map: %{}
   end
 
   def start_link(state) do
@@ -11,6 +16,7 @@ defmodule MixTomboWatch.FsSubscriber do
 
   @impl true
   def init(_state) do
+    create_wrap_command_sh(File.cwd!())
     config = Path.join(File.cwd!(), ".mix_tombo_watch.exs") |> read_config!()
 
     case FileSystem.start_link(dirs: Enum.map(config.dirs, &Path.absname(&1))) do
@@ -27,10 +33,10 @@ defmodule MixTomboWatch.FsSubscriber do
   def handle_info({:file_event, pid, {path, events}}, %State{} = state)
       when is_pid(pid) and is_binary(path) and is_list(events) do
     if match_any_patterns?(path, state.config.patterns) do
-      close_ports(state.ports)
-      ports = run(state.config.commands)
+      close_ports(Map.keys(state.port_map))
+      port_map = run(state.config.commands)
       debounce(state.config.debounce)
-      {:noreply, %State{state | ports: ports}}
+      {:noreply, %State{state | port_map: port_map}}
     else
       {:noreply, state}
     end
@@ -39,6 +45,16 @@ defmodule MixTomboWatch.FsSubscriber do
   @impl true
   def handle_info({port, {:data, data}}, state) when is_port(port) do
     IO.write(data)
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({port, {:exit_status, exit_status}}, state) when is_port(port) do
+    if Map.has_key?(state.port_map, port) do
+      command = Map.get(state.port_map, port)
+      Logger.info("\"#{command}\" exit, status: #{exit_status}")
+    end
+
     {:noreply, state}
   end
 
@@ -51,10 +67,17 @@ defmodule MixTomboWatch.FsSubscriber do
   end
 
   defp run(commands) when is_list(commands) do
-    path_to_wrapper = Application.app_dir(:mix_tombo_watch, ["priv", "wrap_command.sh"])
+    path_to_wrapper = Path.join(File.cwd!(), @wrap_command_sh_file_name)
 
-    Enum.map(commands, fn command ->
-      Port.open({:spawn_executable, path_to_wrapper}, [:binary, args: ["bash", "-c", command]])
+    Enum.reduce(commands, %{}, fn command, acc ->
+      port =
+        Port.open({:spawn_executable, path_to_wrapper}, [
+          :binary,
+          :exit_status,
+          args: ["bash", "-c", command]
+        ])
+
+      Map.put(acc, port, command)
     end)
   end
 
@@ -72,6 +95,12 @@ defmodule MixTomboWatch.FsSubscriber do
 
   defp match_any_patterns?(path, patterns) when is_binary(path) and is_list(patterns) do
     Enum.any?(patterns, &String.match?(path, &1))
+  end
+
+  defp create_wrap_command_sh(path) do
+    path = Path.join(path, @wrap_command_sh_file_name)
+    File.write!(path, @wrap_command_sh)
+    File.chmod!(path, 0o775)
   end
 
   @spec read_config!(path :: String.t()) :: MixTomboWatch.Config.t()
