63bcdf5c220ffd68ad9cd40646c9c5b3a5703375
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-11-07 07:10:01 +0900

    Improve config template comment

--- priv/.fwatch.exs
+++ priv/.fwatch.exs
@@ -1,14 +1,19 @@
 import Config
 
 config :file_watch,
+  # dirs to be watched
   dirs: ["."],
+  # commands to be executed when detected, multiple commands can be specified
   commands: [
     "echo -e \"  using default config file,\n  modify CWD/.fwatch.exs to fit your use case. 👀\n\""
   ],
-  # https://hexdocs.pm/elixir/Regex.html
+  # path detecting patterns, should be written in regex
+  # refs. https://hexdocs.pm/elixir/Regex.html
   patterns: [~r".*"],
+  # if your editor touches multiple files in a short period,
+  # it can be avoided by increasing the debounce(msec)
   debounce: 100
 
 config :logger,
-  # use :debug to check matched path and ran command
+  # use :debug to show matched path and ran command
   level: :none
