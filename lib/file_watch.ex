c8f294fc9f6a95b517f607f7c73648e375636c67
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-11-07 20:35:28 +0900

    Refactor and add tests

--- lib/file_watch.ex
+++ lib/file_watch.ex
@@ -1,12 +1,11 @@
 defmodule FileWatch do
   @shortdoc FileWatch.MixProject.description()
   @moduledoc """
-
   #{@shortdoc} v#{FileWatch.MixProject.version()}
 
   ### Get config template
 
-  config template, #{FileWatch.Config.file_name()}, will be generated under CWD.
+  config template, #{FileWatch.Assets.config_file_name()}, will be generated under CWD.
   configuration details are described in it.
 
       $ fwatch --config-template
@@ -16,42 +15,75 @@ defmodule FileWatch do
       $ fwatch
   """
 
+  alias FileWatch.Assets
+
   def main(args) do
+    assets_dir_path = File.cwd!()
+
     case OptionParser.parse(args, strict: [config_template: :boolean]) do
-      {[], [], []} -> run(self())
-      {[config_template: true], [], []} -> FileWatch.Config.create_template()
-      _ -> help()
+      {[], [], []} ->
+        run(assets_dir_path)
+
+      {[config_template: true], [], []} ->
+        Path.join(assets_dir_path, Assets.config_file_name())
+        |> Assets.create_config_file()
+
+      _ ->
+        help()
     end
   end
 
-  def run(pid) do
-    case FileWatch.Config.read() do
+  @spec run(assets_dir_path :: String.t()) :: :ok
+  def run(assets_dir_path) do
+    Path.join(assets_dir_path, Assets.config_file_name())
+    |> Assets.read_config()
+    |> case do
       {:ok, config} ->
-        FileWatch.Logger.configure(config)
-        FileWatch.Assets.create_wrapper_file()
-        Application.put_env(:file_watch, :main_pid, pid)
-        start_link(config: config)
+        Keyword.get(config, :logger, []) |> Logger.configure()
 
-        if not (Code.ensure_loaded?(IEx) && IEx.started?()) do
-          recieve_exit()
-        end
+        config = Keyword.get(config, :file_watch, [])
+        wrapper_file_path = Path.join(assets_dir_path, Assets.wrapper_file_name())
+        Assets.create_wrapper_file(wrapper_file_path)
+
+        run_impl(config, wrapper_file_path)
 
       _ ->
         :ok
     end
   end
 
-  def recieve_exit() do
-    receive do
-      :exit -> :ok
-    end
+  @doc """
+  run_impl/2 is the essential function of :file_watch,
+  which can be called either as an escript or as a mix task.
+  """
+  @spec run_impl(config :: list(), wrapper_file_path :: String.t()) :: :ok
+  def run_impl(config, wrapper_file_path) when is_list(config) and is_binary(wrapper_file_path) do
+    Application.put_env(:file_watch, :main_pid, self())
+
+    start_link(config: config, wrapper_file_path: wrapper_file_path)
+
+    if on_iex?(), do: :ok, else: receive(do: (:exit -> :ok))
   end
 
-  def exit!() do
+  def exit() do
     Application.fetch_env!(:file_watch, :main_pid) |> send(:exit)
   end
 
-  defp help(), do: IO.puts(@moduledoc)
+  def highlight(binary) when is_binary(binary) do
+    binary |> String.trim_trailing() |> String.split("\n") |> highlight()
+  end
+
+  def highlight(lines) when is_list(lines) do
+    """
+    \n\s\s#{Enum.join(lines, "\n\s\s")}
+    """
+  end
+
+  defp help(), do: @moduledoc |> highlight() |> IO.puts()
+
+  defp on_iex?() do
+    Code.ensure_loaded?(IEx) and IEx.started?()
+  end
 
   use Supervisor
 
--- test/file_watch_test.exs
+++ test/file_watch_test.exs
@@ -1,3 +1,35 @@
 defmodule FileWatchTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
+
+  import ExUnit.CaptureIO
+
+  alias FileWatch.Assets
+
+  describe "run/2" do
+    @tag :tmp_dir
+    test "without config file", %{tmp_dir: tmp_dir_path} do
+      assert capture_io(fn -> FileWatch.run(tmp_dir_path) end) =~ "Please check"
+    end
+
+    @tag :tmp_dir
+    test "with config file", %{tmp_dir: tmp_dir_path} do
+      capture_io(fn ->
+        Path.join(tmp_dir_path, Assets.config_file_name())
+        |> Assets.create_config_file()
+
+        pid = self()
+        send(pid, :exit)
+        FileWatch.run(tmp_dir_path)
+
+        assert Assets.wrapper_file_name() in File.ls!(tmp_dir_path)
+        assert ^pid = Application.fetch_env!(:file_watch, :main_pid)
+      end)
+    end
+  end
+
+  test "exit/0" do
+    Application.put_env(:file_watch, :main_pid, self())
+    FileWatch.exit()
+    assert_receive(:exit)
+  end
 end
