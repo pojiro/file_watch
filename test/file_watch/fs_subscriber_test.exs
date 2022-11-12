61568f8fd10227686104fb44786e7230dcc6f9d9
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-11-12 10:28:44 +0900

    Refactor FsSubscriber

--- test/file_watch/fs_subscriber_test.exs
+++ test/file_watch/fs_subscriber_test.exs
@@ -4,15 +4,15 @@ defmodule FileWatch.FsSubscriberTest do
   alias FileWatch.Assets
   alias FileWatch.FsSubscriber
 
-  test "run/2 single command" do
+  test "run_on_unix/2 single command" do
     wrapper_file_path = Path.join("priv", Assets.wrapper_file_name())
-    port = FsSubscriber.run("echo hello", wrapper_file_path)
+    port = FsSubscriber.run_on_unix("echo hello", wrapper_file_path)
     assert_receive {^port, {:data, "hello\n"}}
   end
 
-  test "run/2 multiple commands" do
+  test "run_on_unix/2 multiple commands" do
     wrapper_file_path = Path.join("priv", Assets.wrapper_file_name())
-    [port_1, port_2] = FsSubscriber.run(["echo hello", "echo world"], wrapper_file_path)
+    [port_1, port_2] = FsSubscriber.run_on_unix(["echo hello", "echo world"], wrapper_file_path)
 
     assert_receive {^port_1, {:data, "hello\n"}}
     assert_receive {^port_2, {:data, "world\n"}}
